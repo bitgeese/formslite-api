4f8da818292e6d14645b6b5fe7bccc60780ed670
Maciej Janowski <maciej.janowski1@icloud.com>
2024-05-23 16:09:31 -0300

    update allowed hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
     default="zJBBSUsc2DkGM9iHpCMSezFnueCcqGgMhgWeseebv3K1yKvchakgtbuBMIbrnqdq",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["*"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
