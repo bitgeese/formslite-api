8ba5ebe30a9abfff6c7883d8a0810e78da7188bb
Maciej Janowski <maciej.janowski1@icloud.com>
2024-05-28 00:43:04 -0300

    update files

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,12 +71,12 @@ DJANGO_APPS = [
     "django.forms",
 ]
 THIRD_PARTY_APPS = [
-    "crispy_forms",
-    "crispy_bootstrap5",
-    "allauth",
-    "allauth.account",
+    # "crispy_forms",
+    # "crispy_bootstrap5",
+    # "allauth",
+    # "allauth.account",
     # "allauth.mfa",
-    "allauth.socialaccount",
+    # "allauth.socialaccount",
     "django_celery_beat",
     "rest_framework",
     "rest_framework.authtoken",
@@ -85,7 +85,6 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "web_forms.users",
     "web_forms.access_keys",
     "web_forms.submissions",
 ]
@@ -104,12 +103,7 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
-# https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
-AUTH_USER_MODEL = "users.User"
-# https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "users:redirect"
-# https://docs.djangoproject.com/en/dev/ref/settings/#login-url
-LOGIN_URL = "account_login"
+
 
 # PASSWORDS
 # ------------------------------------------------------------------------------
@@ -144,7 +138,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
+    # "allauth.account.middleware.AccountMiddleware",
 ]
 
 # STATIC
@@ -190,7 +184,7 @@ TEMPLATES = [
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
-                "web_forms.users.context_processors.allauth_settings",
+                # "web_forms.users.context_processors.allauth_settings",
             ],
         },
     },
@@ -237,7 +231,7 @@ ADMINS = [("""Maciej Janowski""", "maciekjanowski42@icloud.com")]
 MANAGERS = ADMINS
 # https://cookiecutter-django.readthedocs.io/en/latest/settings.html#other-environment-settings
 # Force the `admin` sign in process to go through the `django-allauth` workflow
-DJANGO_ADMIN_FORCE_ALLAUTH = env.bool("DJANGO_ADMIN_FORCE_ALLAUTH", default=False)
+# DJANGO_ADMIN_FORCE_ALLAUTH = env.bool("DJANGO_ADMIN_FORCE_ALLAUTH", default=False)
 
 # LOGGING
 # ------------------------------------------------------------------------------
@@ -296,23 +290,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_SEND_TASK_EVENTS = True
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-task_send_sent_event
 CELERY_TASK_SEND_SENT_EVENT = True
-# django-allauth
-# ------------------------------------------------------------------------------
-ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
-# https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_AUTHENTICATION_METHOD = "username"
-# https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_EMAIL_REQUIRED = True
-# https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-# https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_ADAPTER = "web_forms.users.adapters.AccountAdapter"
-# https://docs.allauth.org/en/latest/account/forms.html
-ACCOUNT_FORMS = {"signup": "web_forms.users.forms.UserSignupForm"}
-# https://docs.allauth.org/en/latest/socialaccount/configuration.html
-SOCIALACCOUNT_ADAPTER = "web_forms.users.adapters.SocialAccountAdapter"
-# https://docs.allauth.org/en/latest/socialaccount/configuration.html
-SOCIALACCOUNT_FORMS = {"signup": "web_forms.users.forms.UserSocialSignupForm"}
+
 
 # django-rest-framework
 # -------------------------------------------------------------------------------
--- web_forms/conftest.py
+++ web_forms/conftest.py
@@ -1,14 +1,14 @@
-import pytest
+# import pytest
 
-from web_forms.users.models import User
-from web_forms.users.tests.factories import UserFactory
-
-
-@pytest.fixture(autouse=True)
-def _media_storage(settings, tmpdir) -> None:
-    settings.MEDIA_ROOT = tmpdir.strpath
+# from web_forms.users.models import User
+# from web_forms.users.tests.factories import UserFactory
 
 
-@pytest.fixture()
-def user(db) -> User:
-    return UserFactory()
+# @pytest.fixture(autouse=True)
+# def _media_storage(settings, tmpdir) -> None:
+#     settings.MEDIA_ROOT = tmpdir.strpath
+
+
+# @pytest.fixture()
+# def user(db) -> User:
+#     return UserFactory()
