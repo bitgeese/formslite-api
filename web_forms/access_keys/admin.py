74f493dc02911d33b440838bf659bd11fae107eb
Maciej Janowski <maciej.janowski1@icloud.com>
2024-06-03 11:17:20 -0300

    changes

--- web_forms/access_keys/admin.py
+++ web_forms/access_keys/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import AccessKey
+from .models import AccessKey, SimpleUser
 
 
 @admin.register(AccessKey)
 class AccessKeyAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "user"]
     search_fields = ["user"]
+
+
+@admin.register(SimpleUser)
+class SimpleUserAdmin(admin.ModelAdmin):
+    list_display = ["email"]
+    search_fields = ["email"]
