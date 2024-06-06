f664fc660fd2301bb387145f5b240256aac4401f
Maciej Janowski <maciej.janowski1@icloud.com>
2024-06-06 10:58:34 -0300

    update exceptions

--- web_forms/submissions/api/fields.py
+++ web_forms/submissions/api/fields.py
@@ -4,17 +4,20 @@ from rest_framework import serializers
 class SemicolonSeparatedEmailField(serializers.Field):
     def to_internal_value(self, data):
         if not isinstance(data, str):
-            raise serializers.ValidationError("This field must be a string.")
+            error_message = "This field must be a string."
+            raise serializers.ValidationError(error_message)
 
         email_list = [email.strip() for email in data.split(";") if email.strip()]
 
         for email in email_list:
             if not serializers.EmailField().run_validation(email):
-                raise serializers.ValidationError(f"Invalid email address: {email}")
+                error_message = f"Invalid email address: {email}"
+                raise serializers.ValidationError(error_message)
 
         return email_list
 
     def to_representation(self, value):
         if not isinstance(value, list):
-            raise serializers.ValidationError("This field must be a list.")
+            error_message = "This field must be a list."
+            raise serializers.ValidationError(error_message)
         return ";".join([x.strip() for x in value])
