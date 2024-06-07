6f960a438a1d4a4be7ab0fe557ff71997f9dce0c
Maciej Janowski <maciej.janowski1@icloud.com>
2024-06-07 16:26:16 -0300

    remove unused import

--- web_forms/dashboard/magic_links.py
+++ web_forms/dashboard/magic_links.py
@@ -3,7 +3,6 @@ from typing import Optional
 from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.urls import reverse
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
